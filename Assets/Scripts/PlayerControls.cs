f020707d3c47b72396e13e00cf1c69b477664834
Jesusrr06 <jrodrem422@g.educaand.es>
2026-03-25 13:38:08 +0100

    advanced a bit

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -1,5 +1,5 @@
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 // ReSharper disable Unity.PerformanceCriticalCodeInvocation
 public class PlayerController : MonoBehaviour
 {
@@ -8,9 +8,12 @@ public class PlayerController : MonoBehaviour
     private PlayerInputController playerInputController;
     private GroundController groundController;
     private Rigidbody rb;
+    public float turnSpeed=100f;
+
     private bool jumpTriggered;
     private void Awake()
     {
+      
         playerInputController = GetComponent<PlayerInputController>();
        groundController = GetComponent<GroundController>();
         rb = GetComponent<Rigidbody>();
@@ -21,12 +24,21 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
+
         Vector3 velocity = new Vector3(playerInputController.MovementInputVector.x, 
              0,
              playerInputController.MovementInputVector.y)  * speed;
-      
+
+        if (velocity != Vector3.zero)
+        {        
+            Quaternion direction = Quaternion.Euler(playerInputController.MovementInputVector.x,0 , playerInputController.MovementInputVector.y );
+transform.rotation = Quaternion.RotateTowards(transform.rotation, direction,turnSpeed * Time.deltaTime);
+        }
+
         velocity.y= rb.linearVelocity.y;
-         if (jumpTriggered)
+    
+
+        if (jumpTriggered)
          {
              velocity.y = jump;
              jumpTriggered = false;

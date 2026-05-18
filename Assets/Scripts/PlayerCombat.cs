8abcc4c3a37f4011c1e377e41d378086e371d0a1
Jesusrr06 <jrodrem422@g.educaand.es>
2026-05-18 13:37:02 +0200

    last change

--- Assets/Scripts/PlayerCombat.cs
+++ Assets/Scripts/PlayerCombat.cs
@@ -1,30 +1,36 @@
-using System;
 using UnityEngine;
 using System.Collections;
 
 public class PlayerCombat : MonoBehaviour
 {
-
     [Header("Hitboxes")]
     public Hitbox hitboxes;
 
     private Animator _animator;
+    private PlayerMovement _movementOwner;
 
+    [Header("Combat State")]
     private bool _isPunching;
     private bool _isKicking;
     private bool _isShooting;
     private bool _isBlocking;
-    private PlayerMovement _movementOwner;
     private bool _isInEndLag;
-    public  int playerId;
+
+    public int playerId;
 
     // ================= GETTERS =================
     public bool IsBlocking => _isBlocking;
 
-    private void Awake()
-    {         _movementOwner = GetComponent<PlayerMovement>();
+    public bool IsAttacking =>
+        _isPunching ||
+        _isKicking ||
+        _isShooting;
 
+    private void Awake()
+    {
+        _movementOwner = GetComponent<PlayerMovement>();
         _animator = GetComponent<Animator>();
+
         FindHitbox();
     }
 
@@ -32,17 +38,12 @@ public class PlayerCombat : MonoBehaviour
     {
         HandlePlayerInput();
         HandleAnimations();
-        
-    }
-    private void HandleAnimations()
-    {
-        _animator.SetBool("IsBlocking", _isBlocking);
-
-        _animator.SetBool("IsStunned", _movementOwner.isStunned);
     }
 
+    // ================= INPUT =================
     private void HandlePlayerInput()
     {
+        // Prevent all combat while stunned
         if (_movementOwner.isStunned)
         {
             SetBlock(false);
@@ -52,34 +53,66 @@ public class PlayerCombat : MonoBehaviour
         // PLAYER 1
         if (playerId == 1)
         {
+            if (Input.GetKeyDown(KeyCode.F))
+                Punch();
 
-            if (Input.GetKey(KeyCode.F)) Punch();
-            if (Input.GetKey(KeyCode.G)) Kick();
-            if (Input.GetKey(KeyCode.H)) Shoot();
+            if (Input.GetKeyDown(KeyCode.G))
+                Kick();
+
+            if (Input.GetKeyDown(KeyCode.H))
+                Shoot();
+
             SetBlock(Input.GetKey(KeyCode.E));
         }
 
         // PLAYER 2
         if (playerId == 2)
         {
-            if (Input.GetKey(KeyCode.Keypad1)) Punch();
-            if (Input.GetKey(KeyCode.Keypad2)) Kick();
-            if (Input.GetKey(KeyCode.Keypad3)) Shoot();
+            if (Input.GetKeyDown(KeyCode.Keypad1))
+                Punch();
+
+            if (Input.GetKeyDown(KeyCode.Keypad2))
+                Kick();
+
+            if (Input.GetKeyDown(KeyCode.Keypad3))
+                Shoot();
+
             SetBlock(Input.GetKey(KeyCode.Keypad4));
         }
-
     }
-    
+
+    // ================= ANIMATIONS =================
+    private void HandleAnimations()
+    {
+        _animator.SetBool("IsBlocking", _isBlocking);
+        _animator.SetBool("IsStunned", _movementOwner.isStunned);
+    }
+
+    // ================= CAN ATTACK =================
+    private bool CanAttack()
+    {
+        return
+            !_isPunching &&
+            !_isKicking &&
+            !_isShooting &&
+            !_isBlocking &&
+            !_isInEndLag &&
+            !_movementOwner.isStunned;
+    }
 
     // ================= PUNCH =================
     private void Punch()
     {
-        if ( _isBlocking) return;
+        Debug.Log("Punch");
+        if (!CanAttack()) return;
 
         _isPunching = true;
+        _isInEndLag = true;
+
         _animator.SetTrigger("IsPunching");
 
         hitboxes.BeginAttack();
+
         StartCoroutine(PunchRoutine());
     }
 
@@ -92,18 +125,27 @@ public class PlayerCombat : MonoBehaviour
         yield return new WaitForSeconds(0.15f);
 
         hitboxes.DisableArm();
+
         _isPunching = false;
+
+        // Endlag
+        yield return new WaitForSeconds(0.2f);
+
+        _isInEndLag = false;
     }
 
     // ================= KICK =================
     private void Kick()
     {
-        if (_isBlocking) return;
+        if (!CanAttack()) return;
 
         _isKicking = true;
+        _isInEndLag = true;
+
         _animator.SetTrigger("IsKicking");
 
         hitboxes.BeginAttack();
+
         StartCoroutine(KickRoutine());
     }
 
@@ -116,35 +158,51 @@ public class PlayerCombat : MonoBehaviour
         yield return new WaitForSeconds(0.17f);
 
         hitboxes.DisableLeg();
+
         _isKicking = false;
+
+        // Endlag
+        yield return new WaitForSeconds(0.25f);
+
+        _isInEndLag = false;
     }
 
     // ================= SHOOT =================
     private void Shoot()
     {
-        if (_isBlocking) return;
+        if (!CanAttack()) return;
 
         _isShooting = true;
+        _isInEndLag = true;
+
         _animator.SetTrigger("IsShooting");
 
-        StartCoroutine(ResetShoot());
+        StartCoroutine(ShootRoutine());
     }
 
-    private IEnumerator ResetShoot()
+    private IEnumerator ShootRoutine()
     {
-        yield return new WaitForSeconds(0.15f);
+        // Spawn projectile here later
+
+        yield return new WaitForSeconds(0.3f);
+
         _isShooting = false;
+
+        // Endlag
+        yield return new WaitForSeconds(0.3f);
+
+        _isInEndLag = false;
     }
 
     // ================= BLOCK =================
     private void SetBlock(bool state)
     {
-        if (_isPunching || _isKicking || _isShooting)
+        // Can't block while attacking
+        if (IsAttacking)
             state = false;
 
         _isBlocking = state;
     }
-    
 
     // ================= HITBOX =================
     private void FindHitbox()
@@ -157,7 +215,6 @@ public class PlayerCombat : MonoBehaviour
             {
                 hitboxes = hb;
 
-                // ✔ actualización correcta para arquitectura nueva
                 hitboxes.movementOwner = GetComponent<PlayerMovement>();
                 hitboxes.combatOwner = this;
 
